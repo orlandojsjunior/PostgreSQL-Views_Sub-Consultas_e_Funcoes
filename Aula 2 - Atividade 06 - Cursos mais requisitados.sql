2c312c59f9ae157a07bd559c597d6db120808f4d
Orlando Junior <orlandojsjunior@hotmail.com>
2024-08-07 20:18:07 -0300

    Relatório - Cursos mais requisitados

--- Aula 2 - Atividade 06 - Cursos mais requisitados.sql
+++ Aula 2 - Atividade 06 - Cursos mais requisitados.sql
@@ -1,9 +1,7 @@
-SELECT aluno.primeiro_nome,
-       aluno.ultimo_nome,
-       COUNT(curso.id) numero_cursos
-    FROM aluno
-    JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-    JOIN curso ON curso.id = aluno_curso.curso_id
-GROUP BY 1,2
-ORDER BY numero_cursos DESC
-	LIMIT 1;
+SELECT curso.nome,
+       COUNT(aluno_curso.aluno_id) numero_alunos
+    FROM curso
+    JOIN aluno_curso ON aluno_curso.curso_id = curso.id
+GROUP BY 1
+ORDER BY numero_alunos DESC
+    LIMIT 1;
